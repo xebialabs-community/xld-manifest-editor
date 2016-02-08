3a4fb811f7a0dadeb2b2950678b1ccc77f15637c
Aditya Kalia <aditya.kalia@gmail.com>
2016-02-08 18:14:23 +0530

    Updating license URL

--- XebiaLabs.XLDeploy/XebiaLabs.XLDeploy.UI/ViewModels/EnumPropertyEntryEditorViewModel.cs
+++ XebiaLabs.XLDeploy/XebiaLabs.XLDeploy.UI/ViewModels/EnumPropertyEntryEditorViewModel.cs
@@ -5,7 +5,7 @@
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
 // There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
 // this software, see the FLOSS License Exception
-// <https://github.com/jenkinsci/deployit-plugin/blob/master/LICENSE>.
+// <https://github.com/xebialabs-community/xld-manifest-editor/blob/master/LICENSE>.
 //
 // This program is free software; you can redistribute it and/or modify it under the terms
 // of the GNU General Public License as published by the Free Software Foundation; version 2
